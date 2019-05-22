e5b2a9f4a873a18fa2a1f5c38b8a03e5828c877a
Aglae Verduzco <aglae.cv@gmail.com>
2019-05-22 01:17:36 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -14,7 +14,7 @@ class Triangle
       raise TriangleError
     rescue TriangleError => error
       puts error.messsage
-    end
+    #end
 
     elsif s1 == s2 && s1 == s3
     self.kind = :equilateral
